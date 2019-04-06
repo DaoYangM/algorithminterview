09ed36611a64d0c109549c3ab829ddc6d1c07b03
daoyang <deyangye@gmail.com>
2019-04-06 21:22:48 +0800

    good

--- src/main/java/top/daoyang/Dev.java
+++
@@ -1,9 +0,0 @@
-package top.daoyang;
-
-public class Dev {
-    public static void main(String[] args) {
-        System.out.println("checkout dev");
-        System.out.println("We do right things");
-        System.out.println("master change");
-    }
-}
