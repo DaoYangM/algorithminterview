b1427cf70d6bd4799bb8fc03d8d24f2a674fb1a0
daoyang <deyangye@gmail.com>
2019-04-06 12:19:55 +0800

    master change

--- src/main/java/top/daoyang/Dev.java
+++ src/main/java/top/daoyang/Dev.java
@@ -4,5 +4,6 @@ public class Dev {
     public static void main(String[] args) {
         System.out.println("checkout dev");
         System.out.println("We do right things");
+        System.out.println("master change");
     }
 }
