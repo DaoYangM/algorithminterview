3b24983bf6db418d5d292248b4143113e97b1d4b
daoyang <deyangye@gmail.com>
2019-04-06 12:11:50 +0800

    right things

--- src/main/java/top/daoyang/Dev.java
+++ src/main/java/top/daoyang/Dev.java
@@ -3,5 +3,6 @@ package top.daoyang;
 public class Dev {
     public static void main(String[] args) {
         System.out.println("checkout dev");
+        System.out.println("We do right things");
     }
 }
