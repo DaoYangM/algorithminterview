9482bcc4a2ef4890c2bb175d67666f5db78dc2aa
daoyang <deyangye@gmail.com>
2019-04-13 12:25:11 +0800

    word pattern

---
+++ src/main/java/top/daoyang/setandmap/WordPattern.java
@@ -0,0 +1,35 @@
+package top.daoyang.setandmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordPattern {
+    public boolean isValid(String pattern, String str) {
+        Map<Character, String> reflect = new HashMap<>();
+
+        char[] pc = pattern.toCharArray();
+        String[] sc = str.split(" ");
+        
+        if (pc.length != sc.length) 
+            return false;
+
+        for (int i = 0; i < pc.length; i++) {
+            String value = reflect.get(pc[i]);
+            if (value != null) {
+                if (!value.equals(sc[i]))
+                    return false;
+            } else {
+                if (reflect.containsValue(sc[i]))
+                    return false;
+                reflect.put(pc[i], sc[i]);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        WordPattern wordPattern = new WordPattern();
+        System.out.println(wordPattern.isValid("abba", "dog cat cat dog"));
+    }
+}
