73f206cf970bc24f7dc8972fef47949c3f50cf4b
daoyang <deyangye@gmail.com>
2019-04-21 23:34:42 +0800

    ContainsDuplicate

---
+++ src/main/java/top/daoyang/setandmap/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+package top.daoyang.setandmap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> dup = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (dup.contains(nums[i]))
+                return true;
+            dup.add(nums[i]);
+        }
+        return false;
+    }
+}
